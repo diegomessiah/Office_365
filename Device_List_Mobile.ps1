102af8599a6b750d5605a3cb841b7cddd2c6b1ff
Diego Messiah <45059187+diegomessiah@users.noreply.github.com>
2020-09-12 13:01:18 +0300

    Update Device_List_Mobile.ps1

--- Device_List_Mobile.ps1
+++ Device_List_Mobile.ps1
@@ -2,14 +2,12 @@
 # Purpose:  List of devices (Tablet & Mobile) connected to Tenant Mailbox
 # Author:   Diego Messiah | https://github.com/diegomessiah	
  
- $credentials = Get-Credential -Credential UserAdmin@Company.com
- Write-Output "Getting the Exchange Online cmdlets"
+$credentials = Get-Credential -Credential Admin@ACME.com
+Write-Output "Getting the Exchange Online cmdlets"
  
-    $session = New-PSSession -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
-        -ConfigurationName Microsoft.Exchange -Credential $credentials `
-        -Authentication Basic -AllowRedirection
-    Import-PSSession $session
- 
+$session = New-PSSession -ConnectionUri https://outlook.office365.com/powershell-liveid/ -ConfigurationName Microsoft.Exchange -Credential $credentials -Authentication Basic -AllowRedirection
+Import-PSSession $session
+
 $csv = ".\MobileDevices.csv"
 $results = @()
 $mailboxUsers = get-mailbox -resultsize unlimited
@@ -18,33 +16,31 @@ $mobileDevice = @()
 foreach($user in $mailboxUsers)
 {
 $UPN = $user.UserPrincipalName
-$displayName = $user.DisplayName
- 
+#Get-MobileDevice
 $mobileDevices = Get-MobileDevice -Mailbox $UPN
        
       foreach($mobileDevice in $mobileDevices)
       {
-          Write-Output "Getting info about a device for $displayName"
+          Write-Output "Getting info about a device for $user"
           $properties = @{
           Name = $user.name
+          UserDisplayName = $mobileDevice.UserDisplayName
           UPN = $UPN
-          DisplayName = $displayName
-          FriendlyName = $mobileDevice.FriendlyName
           ClientType = $mobileDevice.ClientType
-          ClientVersion = $mobileDevice.ClientVersion
-          DeviceId = $mobileDevice.DeviceId
-          DeviceMobileOperator = $mobileDevice.DeviceMobileOperator
           DeviceModel = $mobileDevice.DeviceModel
           DeviceOS = $mobileDevice.DeviceOS
           DeviceTelephoneNumber = $mobileDevice.DeviceTelephoneNumber
-          DeviceType = $mobileDevice.DeviceType
           FirstSyncTime = $mobileDevice.FirstSyncTime
-          UserDisplayName = $mobileDevice.UserDisplayName
+          IsValid = $mobileDevice.IsValid
+          ExchangeObjectId = $mobileDevice.ExchangeObjectId 
+          IsManaged = $mobileDevice.IsManaged
+          IsCompliant = $mobileDevice.IsCompliant
+          IsDisabled = $mobileDevice.IsDisabled
           }
           $results += New-Object psobject -Property $properties
       }
 }
- 
-$results | Select-Object Name,UPN,FriendlyName,DisplayName,ClientType,ClientVersion,DeviceId,DeviceMobileOperator,DeviceModel,DeviceOS,DeviceTelephoneNumber,DeviceType,FirstSyncTime,UserDisplayName | Export-Csv -notypeinformation -Path $csv
- 
+
+$results | Select-Object Name,UserDisplayName,UPN,ClientType,DeviceModel,DeviceOS,DeviceTelephoneNumber,FirstSyncTime,IsValid,ExchangeObjectId,IsManaged,IsCompliant,IsDisabled | Export-Csv -notypeinformation -Path $csv
+
 Remove-PSSession $session
